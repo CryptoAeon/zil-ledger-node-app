3b6bc231c906717604265d927736cb54d098e7b4
Rinat <lich666black@gmail.com>
2020-06-01 17:03:25 +0000

    fixed bip39

--- get-account.js
+++ get-account.js
@@ -1,23 +1,18 @@
 const { Wallet } = require('@zilliqa-js/account/dist/wallet');
 const bip39 = require('bip39');
 const hdkey = require('hdkey');
-
-
 function addByMnemonic(phrase, index) {
   const wallet = new Wallet('')
-
   if (!wallet.isValidMnemonic(phrase)) {
     throw new Error(`Invalid mnemonic phrase: ${phrase}`);
   }
-  const seed = bip39.mnemonicToSeed(phrase);
+  const seed = bip39.mnemonicToSeedSync(phrase);
   const hdKey = hdkey.fromMasterSeed(seed);
   const childKey = hdKey.derive(`m/44'/313'/0'/0/${index}`);
   const privateKey = childKey.privateKey.toString('hex');
   wallet.addByPrivateKey(privateKey);
-
   return wallet;
 }
-
 /**
  * show full account by mnemonic seed.
  * run:
@@ -30,5 +25,4 @@ function main() {
   const account = JSON.stringify(wallet, null, 4)
   console.log(account)
 }
-
 main();
